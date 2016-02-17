19f97cfed9e619e58d88a1bcf779a638fe75b965
hawkwoodye <hawkwoodye@gmail.com>
2016-02-17 10:40:44 -0500

    course

---
+++ db/migrate/20160217151908_create_courses.rb
@@ -0,0 +1,15 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.integer :couse_number
+      t.string :title
+      t.text :description
+      t.string :instructor
+      t.date :start_date
+      t.date :end_date
+      t.boolean :status
+
+      t.timestamps null: false
+    end
+  end
+end
